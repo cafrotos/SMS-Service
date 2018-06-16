0619b776386f51df0a882635293e177108a4ef35
minhphuong99 <38272383+minhphuong99@users.noreply.github.com>
2018-06-16 16:50:39 +0700

    1.0.0

---
+++ test/smsTest.js
@@ -0,0 +1,72 @@
+const request = require('request');
+const expect = require('chai').expect;
+
+describe("Sent sms", () => {
+    it("Không thể xác thực.(sai mã token)", () => {
+        let smsInfo = {
+            phone: "0946669083",
+            shop_receiver: "saotruc.vn",
+            contents: "asdjfhuerjhfa"
+        }
+
+        let options = {
+            url: 'http://localhost:3000/sms',
+            headers: {
+                'token': 'kajhdsfoi'
+            },
+            form: smsInfo
+        };
+
+        request.post(options, (errors, response, body) => {
+            response = response.toJSON();
+            expect(response.statusCode).to.equal(400);
+            done();
+        })
+    })
+
+    it("Thông tin gửi SMS không đúng.(sai thuộc tính)", () => {
+        let smsInfo = {
+            pho: '0946669083',
+            contents: 'asdfoi',
+            shop_receiver: 'asdg'
+        }
+
+        let options = {
+            url: 'http://localhost:3000/sms',
+            headers: {
+                'token': 'qwerty'
+            },
+            form: smsInfo
+        };
+
+        request.post(options, (errors, response, body) => {
+            response = response.toJSON();
+            expect(response.statusCode).to.equal(400);
+            done();
+        })
+        
+    })
+
+    it("Thông tin gửi SMS không đúng.(sai thuộc tính)", () => {
+        let smsInfo = {
+            pho: '0946669083',
+            contents: 'asdfoi',
+            shop_receiver: ''
+        }
+
+        let options = {
+            url: 'http://localhost:3000/sms',
+            headers: {
+                'token': 'qwerty'
+            },
+            form: smsInfo
+        };
+
+        request.post(options, (errors, response, body) => {
+            response = response.toJSON();
+            expect(response.statusCode).to.equal(400);
+            done();
+        })
+        
+    })
+})
